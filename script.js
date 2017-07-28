15eed683add35659c806161f01f472f7f9e6731d
Brian Chan <brian.chan080@gmail.com>
2017-07-28 15:56:53 +0000

    early stages of page development

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-// JavaScript File
-// JavaScript File
+/* global $ */ 
 $( document ).ready(function() {
-
-
 });
+
+$("#search").click(function(){
+   var search = $("#search").val(); 
+   
+if (search === "computer"){
+    $("#search").html("a href= index.html");
+} else {
+    $("#search").html("a href= index.html");
+}
+
+}); 
